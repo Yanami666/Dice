d0b5162376c44479e4c855210ff36251995f89c0
Yanami666 <m13241787330@163.com>
2026-03-25 01:11:19 -0400

    Revert "Merge branch 'main' of https://github.com/Yanami666/Dice"
    This reverts commit ab9327b4c6cde21d3bab176aebd1cecce1206fc3, reversing
    changes made to 4d8732593834107f8dbd72323a29620c01af98ef.

--- Assets/Text/ChocDino/UIFX-TMP/Editor/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-[assembly: System.Reflection.AssemblyCopyright("Chocolate Dinosaur Ltd")]
-[assembly: System.Reflection.AssemblyTitle("UIFX 1.x (TextMeshPro Editor)")]
-[assembly: System.Reflection.AssemblyVersion("1.9.11")]
